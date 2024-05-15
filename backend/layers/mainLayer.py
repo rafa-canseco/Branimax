d33b3bef14902a7803654bbb32057931c4e6e9b5
rafa <rafa.canseco@gmail.com>
2024-05-15 12:17:36 -0600

    not sure

--- backend/layers/mainLayer.py
+++ backend/layers/mainLayer.py
@@ -8,6 +8,17 @@ from flows.confirmFlow import flow_confirm  # Importar el flujo de confirmación
 
 state = BotState()
 
+state_cache = {}
+
+def get_state(from_number):
+    if from_number in state_cache:
+        return state_cache[from_number]
+    else:
+        return BotState()
+
+def update_state(from_number, state):
+    state_cache[from_number] = state
+
 PROMPT_DISCRIMINATOR = """### Historial de Conversación (Vendedor/Cliente) ###
 {HISTORY}
 
@@ -20,19 +31,22 @@ PROMPT_DISCRIMINATOR = """### Historial de Conversación (Vendedor/Cliente) ###
 
 Por favor, clasifica la siguiente conversación según la intención del usuario."""
 
-async def mainMessaging(state: BotState, ai: AIClass, body: str,from_number):
+async def mainMessaging(state: BotState, ai: AIClass, body: str, from_number):
     print(f"Estado actual de confirmation_phase: {state.get('confirmation_phase')}")
+    
     if state.get('confirmation_phase'):
-        response = await flow_confirm(state, ai, body,from_number)
+        response = await flow_confirm(state, ai, body, from_number)
         return response
 
     history = get_history_parse(state)
-    print(history)
+    print(f"Historial: {history}")
     prompt = PROMPT_DISCRIMINATOR.replace('{HISTORY}', history)
 
     prediction = await ai.determine_chat_fn([
         {"role": "system", "content": prompt}
     ])
+
+    print(f"Predicción: {prediction.get('prediction', '')}")
 
     if "HABLAR" in prediction.get("prediction", ""):
         response = sellerFlow(body, state)
@@ -44,8 +58,9 @@ async def mainMessaging(state: BotState, ai: AIClass, body: str,from_number):
             print("confirmation_phase actualizado a True")
         return response
 
-async def register_message_and_process(body: str, state: BotState, ai: AIClass,from_number):
+async def register_message_and_process(body: str, state: BotState, ai: AIClass, from_number):
     handle_history({'role': 'user', 'content': body}, state)
 
-    response = await mainMessaging(state, ai, body,from_number)
+    response = await mainMessaging(state, ai, body, from_number)
+    update_state(from_number, state)  # Asegúrate de actualizar el estado después de procesar el mensaje
     return response
